2cf72cb67b988266b7751825a7e763d7bb2114bd
Brendan Loudermilk <brendan@gophilosophie.com>
2012-05-09 16:10:02 -0700

    Updates Draper and removes old hack. Everything works

--- app/decorators/application_decorator.rb
+++ app/decorators/application_decorator.rb
@@ -1,7 +1,4 @@
 class ApplicationDecorator < Draper::Base
-  def to_model
-    self
-  end
   # Lazy Helpers
   #   PRO: Call Rails helpers without the h. proxy
   #        ex: number_to_currency(model.price)
